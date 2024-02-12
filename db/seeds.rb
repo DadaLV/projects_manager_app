b1654c691899368384fbc779876d5938e3f44568
DadaLV <vynohradova.l.a@gmail.com>
2024-02-12 08:21:29 +0100

    Add readme and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+User.create(email: 'user1@example.com', password: 'password123')
+User.create(email: 'user2@example.com', password: 'password456')
+
+10.times do
+  Project.create(
+    name: Faker::App.name,
+    description: Faker::Lorem.sentence,
+    user_id: rand(1..2)
+  )
+end
+
+Project.all.each do |project|
+  5.times do
+    Task.create(
+      name: Faker::Lorem.word,
+      description: Faker::Lorem.sentence,
+      status: ['new', 'in_progress', 'completed'].sample,
+      project_id: project.id
+    )
+  end
+end
